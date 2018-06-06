045ffc713f2a343eb61a372b64b040d755e479dc
Akash Verma <akash.verma926@gmail.com>
2018-06-06 20:08:07 +0530

    Commented some statement used for debugging purpose

--- grep.pl
+++ grep.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 @input = <STDIN>;
-print @input;
+#print @input;
 foreach $line(@input){
-	print index($line,$ARGV[0]);
-	print $line;
+	#print index($line,$ARGV[0]);
+	#print $line;
 	if(index($line,$ARGV[0])!=-1){
 		print $line;
 	}
